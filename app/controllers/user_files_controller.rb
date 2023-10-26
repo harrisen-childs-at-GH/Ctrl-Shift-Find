c7ab1d8bf4a15ee3ad63e8039fea0421b82e4ccd
one-body-harrisen <harrisen@onebodywellness.com>
2023-10-26 00:06:00 -0500

    Patch: tailwind install successful

--- app/controllers/user_files_controller.rb
+++ app/controllers/user_files_controller.rb
@@ -1,7 +1,11 @@
 class UserFilesController < ApplicationController
   def index
   end
-  
+
+  def show
+    @user_file = UserFile.find(params[:id])
+  end
+
   def new
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
-  get 'user_files/new'
-  get 'user_files/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :user_files
 
   # Defines the root path route ("/")
-   root "user_files#index"
+  #  root "user_files#index"
 end
