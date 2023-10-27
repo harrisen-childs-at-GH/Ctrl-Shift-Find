160c3785969fd8b2b0b85f9f29402b5940f0ee93
one-body-harrisen <harrisen@onebodywellness.com>
2023-10-27 00:19:48 -0500

    patch: very simple user file

--- app/controllers/user_files_controller.rb
+++ app/controllers/user_files_controller.rb
@@ -4,11 +4,18 @@ class UserFilesController < ApplicationController
 
   def show
     @user_file = UserFile.find(params[:id])
+    @file_content = find_file_content.success
   end
 
   def new
   end
 
   def create
+  end
+
+  private
+
+  def find_file_content
+    UserFileServices::AccessUserFileService.call(user_file_path: @user_file.path)
   end
 end
